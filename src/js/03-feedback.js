90d4c75ba4953bb077d9dc906f7b63a78c68edea
Julia <129061308+JuliaStavychna@users.noreply.github.com>
2023-10-03 22:51:17 +0300

    updattes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,32 +13,23 @@ let formData = {
 updateForm();
 
 function handleInput(event) {
-    const { name, value } = event.target;
-    formData[name] = value;
-    localStorage.setItem(FORM_STATE, JSON.stringify(formData));
-  }
-  
-  inputEl.addEventListener('input', throttle(handleInput, 500));
-  messageEl.addEventListener('input', throttle(handleInput, 500));
+  const { name, value } = event.target;
+  formData[name] = value;
+  localStorage.setItem(FORM_STATE, JSON.stringify(formData));
+}
 
-messageEl.addEventListener(
-  'input',
-  throttle(() => {
-    formData.message = messageEl.value;
-    localStorage.setItem(FORM_STATE, JSON.stringify(formData));
-  }, 500)
-);
+form.addEventListener('input', throttle(handleInput, 500));
 
 form.addEventListener('submit', handleSubmit);
 
 function updateForm() {
-    const formDataJSON = localStorage.getItem(FORM_STATE);
+  const formDataJSON = localStorage.getItem(FORM_STATE);
 
-    if (formDataJSON) {
-        const formData = JSON.parse(formDataJSON);
-        inputEl.value = formData.email;
-        messageEl.value = formData.message;
-    }
+  if (formDataJSON) {
+    const formData = JSON.parse(formDataJSON);
+    inputEl.value = formData.email;
+    messageEl.value = formData.message;
+  }
 }
 
 function handleSubmit(event) {
